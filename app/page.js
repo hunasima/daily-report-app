a0e7ab44b856d78f4c2fc7c405b9ae4eb216d946
spz38 <spz38xg9@gmail.com>
2026-07-01 16:57:52 +0900

    fix UI

--- app/page.js
+++ app/page.js
@@ -18,6 +18,7 @@ export default function Home() {
       <input
         value={text}
         onChange={(e) => setText(e.target.value)}
+        placeholder="ここに入力"
       />
 
       <button onClick={add}>
